122237edf3a2c3316372662403443a6fec58a030
C-I_Tom <tom.manahan@hotmail.com>
2022-10-27 16:06:32 +0000

    Add text descriptions in index.js for code in index.js

--- assets/index.js
+++ assets/index.js
@@ -1,23 +1,23 @@
+// Declaring variables to be used in game
 const playerTurn = document.getElementById('player-turn')
 const newGame = document.getElementById('restart')
 const tiles = Array.from(document.getElementsByClassName('tile'))
 const textPlayerX = "X"
 const textPlayerO = "O"
 
-// console.log(tiles)
-
-let currentPlayer = textPlayerX
-let freeTiles = Array(9).fill(null)
-
-// console.log(freeTiles)
-
 const gameStart = () => {
     tiles.forEach(tile => tile.addEventListener('click', tileClicked))
 }
 
+
+// Create an empty array to be filled by current player when clicked
+let currentPlayer = textPlayerX
+let freeTiles = Array(9).fill(null)
+
+// Create a function that checks tile I.D. and current player and then updates that tile with current player text when clicked
 function tileClicked(player) {
     const id = player.target.id
-    // console.log(id)
+
     if (freeTiles[id] == null) {
         freeTiles[id] = currentPlayer
         player.target.innerText = currentPlayer
