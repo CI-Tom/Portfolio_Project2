5aeecff4bd04a404f192e7a8e04ef6549a9e0ef5
C-I_Tom <tom.manahan@hotmail.com>
2022-10-27 12:39:11 +0000

    Edit sections height to min-height in style.css to prevent overlapping on smaller screen sizes

---
+++ assets/index.js
@@ -0,0 +1,5 @@
+const playerTurn = document.getElementById('player-turn')
+const newGame = document.getElementById('restart')
+const tiles = Array.from(document.getElementsByClassName('game-tile'))
+
+console.log(tiles)
