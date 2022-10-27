e90c489ac67cdff6de5bada16d1988b3c9593f2a
C-I_Tom <tom.manahan@hotmail.com>
2022-10-27 13:16:23 +0000

    Add variables andeventlistener to create array for clicked tiles and add function to target tile id and update current player

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,28 @@
 const playerTurn = document.getElementById('player-turn')
 const newGame = document.getElementById('restart')
-const tiles = Array.from(document.getElementsByClassName('game-tile'))
+const tiles = Array.from(document.getElementsByClassName('tile'))
+const textPlayerX = "X"
+const textPlayerO = "O"
 
-console.log(tiles)
+// console.log(tiles)
+
+let currentPlayer = textPlayerX
+let freeTiles = Array(9).fill(null)
+
+// console.log(freeTiles)
+
+const gameStart = () => {
+    tiles.forEach(tile => tile.addEventListener('click', tileClicked))
+}
+
+function tileClicked(player) {
+    const id = player.target.id
+    // console.log(id)
+    if (freeTiles[id] == null) {
+        freeTiles[id] = currentPlayer
+        player.target.innerText = currentPlayer
+        currentPlayer = currentPlayer == textPlayerX ? textPlayerO : textPlayerX
+    }
+}
+
+gameStart();
